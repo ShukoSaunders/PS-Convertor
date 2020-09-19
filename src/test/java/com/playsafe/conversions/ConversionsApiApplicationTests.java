b4f246cd693f9445a34bbbeaa71b77f8299ad43b
Saunders <saunders.ndhlovu@seacom.mu>
2020-09-19 15:20:26 +0200

    initial commit

---
+++ src/test/java/com/playsafe/conversions/ConversionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playsafe.conversions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
